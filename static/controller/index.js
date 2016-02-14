94d8c7435eedd34eb9fcfe6eba54dee769a3061b
onlyfu <fuwy@foxmail.com>
2016-02-15 00:43:34 +0800

    整合UnjsSp

---
+++ static/controller/index.js
@@ -0,0 +1,23 @@
+/**
+ * 测试控制器
+ */
+
+var index = function(unjs) {
+
+	this.index = function() {
+	
+		var _html = '<p>UnJsSp - 基于JQuery和HTML5的SinglePage框架, <a href="javascript:;" id="link_page">打开另一个页面</a></p>';
+
+		unjs.display(_html);
+
+		// 监听
+        $('#link_page').click(function() {
+
+            unjs.loadControl('post', 'index');
+        });
+	};
+
+	return this;
+};
+
+Module.index = index;

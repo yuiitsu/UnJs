00a4afbcf6ff0e7b13eb44b1934448dc529f0bca
onlyfu <fuwy@foxmail.com>
2016-02-15 12:42:32 +0800

    update demo

--- static/controller/index.js
+++ static/controller/index.js
@@ -6,7 +6,7 @@ var index = function(unjs) {
 
 	this.index = function() {
 	
-		var _html = '<p>UnJsSp - 基于JQuery和HTML5的SinglePage框架, <a href="javascript:;" id="link_page">打开另一个页面</a></p>';
+		var _html = '<p>UnJsSp - 基于HTML5的SinglePage框架, <a href="javascript:;" id="link_page">打开另一个页面</a></p>';
 
 		unjs.display(_html);
 

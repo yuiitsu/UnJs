ead9126cb12c394a7f3c227d0d63e58bd4ba0753
Yuiitsu <fuwy@foxmail.com>
2019-03-04 11:07:22 +0800

    Update Demo

---
+++ dev/component/button/script.js
@@ -0,0 +1,17 @@
+/**
+ *  Button Component
+ */
+Component.extend('button', function() {
+
+    /**
+     * 事件绑定，被调用时自动执行
+     */
+    this.bindEvent = function() {
+    };
+
+    /**
+     * 不指定方法，默认执行方法
+     */
+    this.init = function() {
+    };
+});
